fff4fba3a8f13c65f2327f43e639b257a8e8d7d7
JoãoVitor <joaovitor01990@gmail.com>
2025-07-07 17:23:55 -0300

    Criar programa2.java

---
+++ programa2.java
@@ -0,0 +1,16 @@
+Tabuada simples->
+import java.util.Scanner;
+public class Program
+{
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    int result;
+    
+    System.out.println("Tabuada do "+num+" :");
+    for(int i = 1; i<=10; i++){
+        result = i*num;        
+        System.out.println(num+" × "+ i +" = "+result );
+    }    
+    }
+}
