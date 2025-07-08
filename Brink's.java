44418026aa81b8b63368a4558d526e404cd17e76
JoãoVitor <joaovitor01990@gmail.com>
2025-07-08 18:06:22 -0300

    Create Brink's.java

---
+++ Brink's.java
@@ -0,0 +1,9 @@
+/*Atividade: Crie uma constante para a gravidade (9.8) e mostre-a no console.*/
+public class Program
+{
+    public static void main(String[] args) {
+        final double gravidade = 9.8;
+        
+        System.out.println("A gravidade da terra é "+gravidade);
+    }
+}
