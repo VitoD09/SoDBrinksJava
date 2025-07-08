a1180a1f6e928fcb8d2b339bcc820360344f5b12
JoãoVitor <joaovitor01990@gmail.com>
2025-07-08 18:01:58 -0300

    Create Brinks.java

---
+++ Brinks.java
@@ -0,0 +1,13 @@
+/*Atividade: Declare uma variável de cada tipo e exiba os valores no console.*/
+public class Program
+{
+    public static void main(String[] args) {
+        String nome = "Joao vitor";
+        int idade = 16;
+        double altura = 1.75;
+        char inicial = 'J';
+        boolean estudante = true;
+        
+        System.out.println(nome+" idade "+idade+" altura "+altura+" inicial do nome "+inicial+" é estudante? "+estudante);
+    }
+}
