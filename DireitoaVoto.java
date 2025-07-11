597683c5a9b818b3adf8b5e950a5d2385c31ca45
JoãoVitor <joaovitor01990@gmail.com>
2025-07-11 20:05:35 -0300

    Create DireitoaVoto.java

---
+++ DireitoaVoto.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Program
+{
+    public static void main(String[] args) {
+        int idade;
+        String nome;
+        
+        System.out.println("Informe seu nome: ");
+        Scanner sc = new Scanner(System.in);
+        nome = sc.nextLine();
+        
+        System.out.println("Informe sua idade: ");
+        idade = sc.nextInt();
+        
+        if ((idade >= 18)&&(idade<=70)){
+            System.out.println("Você deve votar!");
+        }else if((idade >= 16) && (idade<18) || (idade>70)){
+            System.out.println("Você pode votar!(opicional)");
+        }
+        else{
+            System.out.println("Você não pode votar!");
+        }
+    }
+}
