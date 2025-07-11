79d406f75c36360934aae2059c23d044f1a8a4ed
JoãoVitor <joaovitor01990@gmail.com>
2025-07-11 20:17:56 -0300

    Create Emprestimo.java

---
+++ Emprestimo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Program
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float salario,emprestimo,prestacao;
+        
+        System.out.println("Informe o valor do salario: ");
+        salario = sc.nextFloat();
+        System.out.println("Informe o valor da prestaçao: ");
+        prestacao  = sc.nextFloat();
+        
+        if(prestacao > 0.2 * salario ){
+            System.out.println("Emprestimo nao concedido!");
+        }else{
+            System.out.println("Emprestimo concedido!");
+        }
+    }
+}
