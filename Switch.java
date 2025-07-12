27c368c08d820f6670b03bc058f0720459386352
JoãoVitor <joaovitor01990@gmail.com>
2025-07-12 17:08:50 -0300

    Update Switch.java

--- Switch.java
+++ Switch.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main{
+public class Switch{
      public static void main (String[] args) {
         
         System.out.println("Informe um numero de 1 a 12");
